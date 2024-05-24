18b56d96a81a4360387cb22f4234dfd63154a594
Wallison <wallisonzwka@gmail.com>
2024-05-23 21:22:24 -0300

    feat/social-auth

--- twitter/urls.py
+++ twitter/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('accounts.urls')),
     path("", include('tweets.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/', include('allauth.urls')),
+]
